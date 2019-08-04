7919dea8e725a2809b8c6f32b5864c69d47af95f
Marton Blum <contact@blummarci.com>
2019-08-04 22:41:50 +0100

    clean

--- class-wc-gateway-barion.php
+++ class-wc-gateway-barion.php
@@ -69,9 +69,6 @@ class WC_Gateway_Barion extends WC_Payment_Gateway {
     static $log = null;
 
     public static function log($message, $level = 'error') {
-        $date = new DateTime();
-        $date = $date->format("r");
-        error_log($date.': '.$message."\n", 3, "/Users/martonblum/off/log/wcs_barion_fork_info2.log");
         if ($level != 'error' && !self::$debug_mode) {
             return;
         }
