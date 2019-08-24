42d65423bbe61264ffa16b32c9633bedaf5b1127
Marton Blum <contact@blummarci.com>
2019-08-24 20:01:32 +0100

    Agree recurring payment process checkbox

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: Barion Payment Gateway for WooCommerce and WooCommerce Subscription
 Plugin URI: https://github.com/bmarci/woocommerce-barion
 Description: Adds the ability to WooCommerce to pay via Barion
-Version: 2.6.0
+Version: 2.6.1
 Author: <a href="http://szelpeter.hu">Peter Szel</a>, <a href="https://blummarci.com">Marton Blum</a>
 Author URI: http://szelpeter.hu
 License: GNU General Public License v3.0
@@ -62,6 +62,52 @@ function wcs_barion_scheduled_subscription($subscription_id) { // TODO: refactor
 
 }
 add_action('woocommerce_scheduled_subscription_payment', 'wcs_barion_scheduled_subscription');
+
+
+/**
+ * Add checkbox field to the checkout
+ **/
+add_action('woocommerce_after_order_notes', 'my_custom_checkout_field');
+
+function my_custom_checkout_field($checkout)
+{
+
+    echo '<div id="recurring_payment_agree_container">';
+
+    woocommerce_form_field('recurring_payment_agree_checkbox', array(
+        'type' => 'checkbox',
+        'class' => array('input-checkbox'),
+        'label' => __('I agree recurring payments.', 'pay-via-barion-for-woocommerce'),
+        'required' => true,
+    ), $checkout->get_value('recurring_payment_agree_checkbox'));
+
+    echo '</div>';
+}
+
+/**
+ * Process the checkout
+ **/
+add_action('woocommerce_checkout_process', 'my_custom_checkout_field_process');
+
+function my_custom_checkout_field_process()
+{
+    global $woocommerce;
+
+    // Check if set, if its not set add an error.
+    if (!$_POST['recurring_payment_agree_checkbox'])
+        wc_add_notice(__('Please agree recurring payments.', 'pay-via-barion-for-woocommerce'), 'error');
+}
+
+/**
+ * Update the order meta with field value
+ **/
+add_action('woocommerce_checkout_update_order_meta', 'my_custom_checkout_field_update_order_meta');
+
+function my_custom_checkout_field_update_order_meta($order_id)
+{
+    if ($_POST['my_checkbox']) update_post_meta($order_id, 'My Checkbox', esc_attr($_POST['my_checkbox']));
+}
+
 
 
 
